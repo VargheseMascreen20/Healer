2fe4e7e64524df274cd7cf75f5d6ed701c8fecb2
VARGHESE T MASCREEN <varghese.t.mascreen20@gmail.com>
2023-01-02 16:51:22 +0530

    Initial Commit

---
+++ Healer/HealerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealerApp.swift
+//  Healer
+//
+//  Created by VARGHESE T MASCREEN on 02/01/23.
+//
+
+import SwiftUI
+
+@main
+struct HealerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
